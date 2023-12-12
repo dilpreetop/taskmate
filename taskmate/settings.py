b227f0ec16b48dce3ca0bed8bbce5d209a2bd292
dilpreetop <dilpreetsingh8974@gmail.com>
2023-12-12 23:04:42 +0530

    db

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -140,5 +140,5 @@ LOGIN_URL='login'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
