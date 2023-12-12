3b8d6aa1d3052cb89f3c12758735cb2533bf18c3
dilpreetop <dilpreetsingh8974@gmail.com>
2023-12-13 01:03:43 +0530

    i

--- taskmate/urls.py
+++ taskmate/urls.py
@@ -19,9 +19,9 @@ from django.urls import path , include
 from todolist_app import views as todolist_views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('task/',include('todolist_app.urls')),
-    path('account/',include('users_app.urls')),
-    path('', todolist_views.index, name='index'),# we are calling home page as index
+    path('', todolist_views.index, name='index'),
+    path('todolist/',include('todolist_app.urls')),
+    path('account/',include('users_app.urls')),# we are calling home page as index
     path('about', todolist_views.about, name='about'),
     path('contact',todolist_views.contact, name='contact'),
    
