2e3939b8d233bbb638f9bc3b9a03271d4b150968
dilpreetop <dilpreetsingh8974@gmail.com>
2023-12-13 13:31:02 +0530

    q

--- users_app/views.py
+++ users_app/views.py
@@ -9,9 +9,8 @@ def register(request):
         if register_form.is_valid():
          register_form.save()
          messages.success(request,("A NEW USER ACCOUNT CREATED,PLEASE LOGIN"))
-        return redirect('register')
+         return redirect('register')
         
     else:
-     register_form= CustomRegisterForm()
-     
+     register_form= CustomRegisterForm()  
     return render(request, 'register.html',{'register_form':register_form})
